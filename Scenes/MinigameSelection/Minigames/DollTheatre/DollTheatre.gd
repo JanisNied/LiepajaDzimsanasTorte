2a5f654758cd2d2d5ea5e5e4f5eb5ee4f36a7a0a
Liviatans <143333830+1ivyatan@users.noreply.github.com>
2024-03-12 22:56:26 +0200

    beat reporting
    yeah i aint using that audiomanager because it has missing thing that i need for conductor

--- Scenes/MinigameSelection/Minigames/DollTheatre/DollTheatre.gd
+++ Scenes/MinigameSelection/Minigames/DollTheatre/DollTheatre.gd
@@ -1,20 +1,16 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	SoundManager.stop("sunMenuMusic")
 	$Orchestra.start_play(4, $Beatmap.getbpm())
-	#SoundManager.play_bgm("dollBeatmapSong")
-	#$Orchestra.begin_orchestra(4, 120)
+
+
+
+func _on_Orchestra_nextbeat(pos):
+	print(pos)
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Orchestra_nextwave(turn):
+	print("NEXT TURN! ", turn)
+	pass # Replace with function body.
