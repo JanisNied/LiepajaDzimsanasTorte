82befbd4502bb310f9f3f245a4b8b993f72f7809
Liviatans <143333830+1ivyatan@users.noreply.github.com>
2024-03-13 18:36:21 +0200

    it falls!

--- Scenes/MinigameMenu/Minigames/DollTheatre/HugeCurtain.gd
+++ Scenes/MinigameMenu/Minigames/DollTheatre/HugeCurtain.gd
@@ -1,4 +1,16 @@
 extends Control
 
+var landed = true
+
+func _physics_process(delta):
+	if landed:
+		return
+	
+	if rect_position.y < 0:
+		rect_position.y += 576 * delta
+	else:
+		rect_position.y == 0
+		landed = true
+
 func _on_Beatmap_eof():
-	pass # Replace with function body.
+	landed = false
