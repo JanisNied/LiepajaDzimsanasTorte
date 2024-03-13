4eac3e225acc7c3c3f88dd35f04d36f62e603c3d
Liviatans <143333830+1ivyatan@users.noreply.github.com>
2024-03-13 10:04:19 +0200

    the dolls now do show up according to beatmap

--- Scenes/MinigameSelection/Minigames/DollTheatre/DollTheatre.gd
+++ Scenes/MinigameSelection/Minigames/DollTheatre/DollTheatre.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var computerTurn
+var step = 0
 
 onready var dollNodes = [$Foreground/Dolls/DollShow/D1, $Foreground/Dolls/DollShow/D2, $Foreground/Dolls/DollShow/D3, $Foreground/Dolls/DollShow/D4]
 
@@ -8,8 +9,24 @@ func _ready():
 	SoundManager.stop("sunMenuMusic")
 	$Orchestra.start_play(5, $Beatmap.getbpm())
 	
+func _unhandled_input(event):
+	if !$Orchestra.playing:
+		return
+	
+	if !event.is_echo() and event.scancode == KEY_SPACE:
+		if computerTurn == 1:
+			print("Miss!")
+	
+	pass
+	
 # conductor
 func _on_Orchestra_nextbeat(pos):
+	self.step = pos
+	
+	if computerTurn == 1:
+		if $Beatmap.dolls[step] == 1:
+			dollNodes[step].visible = true
+	
 	#print(pos)
 	pass # Replace with function body.
 
@@ -17,8 +34,9 @@ func _on_Orchestra_nextwave(turn):
 	$Beatmap.nextLine()
 	self.computerTurn = turn
 	
-	for i in 4:
-		dollNodes[i].visible = false 
+	if computerTurn == 1:
+		for i in 4:
+			dollNodes[i].visible = false 
 	
 	print($Beatmap.dolls)
 	pass # Replace with function body.
