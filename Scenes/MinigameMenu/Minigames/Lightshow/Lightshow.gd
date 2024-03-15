80990dd6989cc54b5326d06076c362373b01b81b
Liviatans <143333830+1ivyatan@users.noreply.github.com>
2024-03-15 17:48:05 +0200

    the dawgs are preparing...

--- Scenes/MinigameMenu/Minigames/Lightshow/Lightshow.gd
+++ Scenes/MinigameMenu/Minigames/Lightshow/Lightshow.gd
@@ -3,6 +3,7 @@ extends Control
 # enemy stages
 var bosses
 var bossidx = 0
+var stop = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,9 +14,15 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	if (stop):
+		return
+	
 	var currenemy
 	if bossidx == 0:
 		currenemy = bosses[bossidx].find_node("RingEnemy")
+		
+		if !currenemy.visible:
+			currenemy.visible = true
 		
 		if bosses[bossidx].offset < 2.5:
 			currenemy.spawnV()
@@ -29,3 +36,10 @@ func _physics_process(delta):
 	
 	#bosses[bossidx].position.move_toward(Vector2(100, 100), delta * 100)
 	pass
+
+
+func _on_Timer_timeout():
+	$Playground/Fighters.visible = false
+	$Playground/Player.visible = true
+	stop = false
+	pass # Replace with function body.
